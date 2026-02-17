add8d7f1372e26fee447617eefcfc12715e5eac5
jeffyzxc <jeffersonnilo24@gmail.com>
2026-02-17 14:31:28 +0800

    - added middleware in chat message - fixes to npm install:all

--- backend/modules/ai-chat-message/chat-message.routes.js
+++ backend/modules/ai-chat-message/chat-message.routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { ChatController } from './chat-message.controller.js'
+import { authenticateJWT } from '../../middleware/auth.middleware.js'
 
 const router = express.Router()
 
-router.post('/', ChatController.sendMessage)
-router.get('/getAll/:id', ChatController.getAllChat)
+router.post('/', authenticateJWT, ChatController.sendMessage)
+router.get('/getAll/:id', authenticateJWT, ChatController.getAllChat)
 
 
 export default router
